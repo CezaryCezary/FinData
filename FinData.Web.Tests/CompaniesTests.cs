2a55780871b4b8822585cf9e177643012d92151c
CezaryCezary <cezarywronka@gmail.com>
2016-03-13 14:44:57 +0100

    Add new test project for Web

---
+++ FinData.Web.Tests/CompaniesTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FinData.Web.Tests
+{
+    [Trait("category", "acceptance")]
+    public class CompaniesTests
+    {
+        [Fact]
+        public void NoCompaniesInTheSystem()
+        {
+            //TODO: implement me
+        }
+    }
+}
